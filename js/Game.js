acdcc4e6b948b813921c212bcfb0bc9d8c34ff01
ajensen415 <ajensen415@yahoo.com>
2021-07-07 20:29:21 -0400

    steps 1-7

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game { 
+    constructor() {
+        this.missed = 0; 
+        this.phrases = [
+            {phrase: "you can do this"}, 
+            {phrase: "no pressure no diamonds"}, 
+            {phrase: "he who is brave is free"}, 
+            {phrase: "see the good"}, 
+            {phrase: "if not now when"}
+            ];
+        this.activePhrase = 'null';
+    }
+    startGame() { 
+        let overlay = document.getElementById("overlay");
+        overlay.style.display = 'none';
+        this.activePhrase = this.getRandomPhrase();
+        let phrase = new Phrase(this.activePhrase.phrase);
+        phrase.addPhraseToDisplay();
+    }
+    getRandomPhrase() {
+        const randomPhrase = Math.floor( Math.random() * this.phrases.length);
+        return this.phrases[randomPhrase];
+    }
+    handleInteraction() {
+
+    }
+    removeLife() {
+
+    }
+    checkForWin() {
+
+    }
+    gameOver() {
+
+    }
+}
+
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phraseText) {
+        console.log(phraseText);
+        this.phrase = phraseText.toLowerCase();
+    }
+    addPhraseToDisplay() {
+        const ul = document.getElementById('phrase').children[0];
+        for (let i = 0; i < this.phrase.length; i++) {
+            let li = document.createElement('li');
+            ul.appendChild(li);
+            li.innerHTML = this.phrase[i];
+            if (this.phrase[i] == " ") {
+                li.className += "space";
+            } else {
+                //li.className += `hide letter ${this.phrase[i]}`;
+                li.className += `letter ${this.phrase[i]}`;
+            }
+        }
+
+    }
+    checkLetter() {
+        const letter = document.getElementsByClassName("key");
+        if (letter == this.phrase[i]) {
+
+        }
+    }
+    showMatchedLetter() {
+
+    }
+}
+
